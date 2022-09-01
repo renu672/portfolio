4767541d4581f539b180462f020f8432e620e1be
renukashyap <renukashyap334@gmail.com>
2022-09-01 22:51:02 +0530

    firstcommit

---
+++ jss/javs.js
@@ -0,0 +1,15 @@
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+    const inputValue = searchInput.value;
+    alert(inputValue);
+});
+$(document).ready(function(e) {
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#", "");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
+});
